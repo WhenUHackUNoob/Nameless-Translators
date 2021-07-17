30ab9552b3bc4a0692dfc8393500bef16e184cef
Supercrafter100 <stijn.voeten@telenet.be>
2021-07-17 21:32:23 +0200

    redo dist

--- dist/handlers/LanguageManager/LanguageManager.js
+++ dist/handlers/LanguageManager/LanguageManager.js
@@ -35,6 +35,11 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -68,20 +73,28 @@ var LanguageManager = /** @class */ (function () {
             placeholders[_i - 2] = arguments[_i];
         }
         return __awaiter(this, void 0, void 0, function () {
-            var data, languageCode, languageData, keys, result, _a, keys_1, key_1, i, k, v;
+            var languageCode, data, languageData, keys, result, _a, keys_1, key_1, i, k, v;
             return __generator(this, function (_b) {
                 switch (_b.label) {
-                    case 0: return [4 /*yield*/, LanguageModel_1.default.findOne({ where: { userID: user } })];
-                    case 1:
-                        data = _b.sent();
-                        if (!!data) return [3 /*break*/, 3];
-                        return [4 /*yield*/, LanguageModel_1.default.create({ userID: user, language: 'en' })];
+                    case 0:
+                        if (!(user == "fallback")) return [3 /*break*/, 1];
+                        languageCode = "en_UK";
+                        return [3 /*break*/, 5];
+                    case 1: return [4 /*yield*/, LanguageModel_1.default.findOne({ where: { userID: user } })];
                     case 2:
                         data = _b.sent();
-                        _b.label = 3;
+                        if (!!data) return [3 /*break*/, 4];
+                        return [4 /*yield*/, LanguageModel_1.default.create({ userID: user, language: 'en_UK' })];
                     case 3:
+                        data = _b.sent();
+                        _b.label = 4;
+                    case 4:
                         languageCode = data.get('language');
-                        languageData = this.languages.get(languageCode.toLowerCase());
+                        _b.label = 5;
+                    case 5:
+                        languageData = this.languages.get(languageCode);
+                        if (!languageData)
+                            return [2 /*return*/, this.getString.apply(this, __spreadArray(["fallback", key], placeholders))];
                         keys = key.split('.');
                         result = languageData;
                         for (_a = 0, keys_1 = keys; _a < keys_1.length; _a++) {
@@ -89,7 +102,7 @@ var LanguageManager = /** @class */ (function () {
                             result = result[key_1];
                             if (!result) {
                                 console.log(Prefixes_1.default.LANGUAGE + ("No language key found for: " + key_1));
-                                return [2 /*return*/, undefined];
+                                return [2 /*return*/, this.getString.apply(this, __spreadArray(["fallback", key_1], placeholders))];
                             }
                         }
                         for (i = 0; i < placeholders.length; i += 2) {
@@ -103,6 +116,27 @@ var LanguageManager = /** @class */ (function () {
         });
     };
     LanguageManager.languages = new discord_js_1.Collection();
+    LanguageManager.languageMap = {
+        "Czech": "cs_CZ",
+        "German": "de_DE",
+        "Greek": "el_GR",
+        "EnglishUK": "en_UK",
+        "EnglishUS": "en_US",
+        "Spanish": "es_419",
+        "SpanishES": "es_ES",
+        "French": "fr_FR",
+        "Hungarian": "hu_HU",
+        "Italian": "it_IT",
+        "Lithuanian": "lt_LT",
+        "Norwegian": "nb_NO",
+        "Dutch": "nl_NL",
+        "Polish": "pl_PL",
+        "Romanian": "ro_RO",
+        "Russian": "ru_RU",
+        "Slovak": "sk_SK",
+        "Turkish": "tr_TR",
+        "Chinese(Simplified)": "zh_CN",
+    };
     return LanguageManager;
 }());
 exports.default = LanguageManager;
