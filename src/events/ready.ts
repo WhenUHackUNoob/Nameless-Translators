041fc0f222c44cc60fd53b4b317a4a863b2fa0a8
When U Hack U Noob <whenuhackunoob@gmail.com>
2021-07-18 10:18:07 +0100

    Set bot status on startup

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,15 @@
 import { client } from "..";
 import Prefixes from "../constants/Prefixes";
 import LanguageModel from "../db/model/LanguageModel";
+import fetch from "node-fetch";
 
 export default {
 	name: 'ready',
 	once: false,
-	run() {
+	async run() {
+
+		// Set status of bot
+		client.user?.setActivity('Translations!', {type: 'WATCHING'})
 
 		// Logging that the bot is ready
 		console.log(Prefixes.BOT + client.user?.tag + " is ready!");
